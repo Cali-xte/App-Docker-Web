a6e47f2a6c0b409cd39a228c8616535bc312cdd0
Calixte <41114634+Cali-xte@users.noreply.github.com>
2021-04-07 20:26:52 +0200

    Update app.sh

--- app.sh
+++ app.sh
@@ -11,9 +11,9 @@ echo "RUN pip install flask" >> ServWeb/Dockerfile
 echo "COPY ./static /home/myapp/static" >> ServWeb/Dockerfile
 echo "COPY ./templates /home/myapp/templates/" >> ServWeb/Dockerfile
 echo "COPY app.py /home/myapp/" >> ServWeb/Dockerfile
-echo "EXPOSE 8080" >> ServWeb/Dockerfile
+echo "EXPOSE 8092" >> ServWeb/Dockerfile
 echo "CMD python3 /home/myapp/app.py" >> ServWeb/Dockerfile
 cd ServWeb
 docker build -t sampleapp .
-docker run -t -d -p 8080:8080 --name samplerunning sampleapp
+docker run -t -d -p 8092:8092 --name samplerunning sampleapp
 docker ps -a
